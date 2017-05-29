d7467d64e40dcd27ef97fd89f0632102a990389b
Michael Furoyama <furoyama51@gmail.com>
2017-05-28 22:12:34 -1000

    Remove unused global regexes

--- AbstractSyntaxTree.js
+++ AbstractSyntaxTree.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const OPERATOR_REGEX = /^(-|\+|\*{1,2}|\\|\/|#|&|!|_|\?|'*<=*|'*>=*|'*\[|'*\]{1,2}|\(|\)|'*=|'|\${1,2}|\^|,|:)/;
-const STRING_REGEX = /^("+)/;
-const CHARACTER_REGEX = /^([A-Za-z0-9\.]+)/;
-
 class AbstractSyntaxTree {
     constructor(tokens) {
         this.tokens = tokens;
