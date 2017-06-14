41bc079e4a82ab01ae48299d5af314955a29fb48
Mike Furoyama <mfuroyama@hawaiirg.com>
2017-06-13 14:19:09 -1000

    Move the tokenization into its own function

--- tokenizer/Line.js
+++ tokenizer/Line.js
@@ -60,6 +60,10 @@ class Line {
         this.text = ((index < this.text.length) ? this.text.slice(index) : '');
         this.column = index;
     }
+    tokenize() {
+        this.chunks = this.createChunks();
+        this.tokens = _.flatten(this.chunks.map(this.createTokens.bind(this)));
+    }
     createChunks() {
         // We want to split a line of text by spaces, but we also want to ignore the split if
         // those spaces are caught in-between quotation marks. So we implement our own special
@@ -130,110 +134,104 @@ class Line {
         }
         return chunks;
     }
-    tokenize() {
-        this.chunks = this.createChunks();
-        // console.log(this.chunks);
-        this.tokens = this.chunks.map((chunk) => {
-            const tokens = [];
-            let currChunk = chunk.value;
-            let column = chunk.column;
+    createTokens(chunk) {
+        const tokens = [];
+        let currChunk = chunk.value;
+        let column = chunk.column;
 
-            while (currChunk.length > 0) {
-                // Perform a regex match search for valid MUMPS operators.
-                const operatorMatch = currChunk.match(OPERATOR_REGEX);
-                if (operatorMatch) {
-                    const value = operatorMatch[0];
-                    tokens.push({
-                        type: 'Operator',
-                        value,
-                        row: this.row,
-                        column,
-                        level: this.indentation,
-                    });
+        while (currChunk.length > 0) {
+            // Perform a regex match search for valid MUMPS operators.
+            const operatorMatch = currChunk.match(OPERATOR_REGEX);
+            if (operatorMatch) {
+                const value = operatorMatch[0];
+                tokens.push({
+                    type: 'Operator',
+                    value,
+                    row: this.row,
+                    column,
+                    level: this.indentation,
+                });
 
-                    currChunk = currChunk.slice(value.length);
-                    column += value.length;
-                }
+                currChunk = currChunk.slice(value.length);
+                column += value.length;
+            }
 
-                // Perform a regex match search for alphanumeric characters at the start of the
-                // current string. If we found something, we save the value as either a literal
-                // or a number, depending on an 'isNaN' check.
-                const charMatch = currChunk.match(CHARACTER_REGEX);
-                if (charMatch) {
-                    const value = charMatch[0];
-                    const type = isNaN(value) ? 'Literal' : 'Numeric';
-                    tokens.push({
-                        type,
-                        value,
-                        row: this.row,
-                        column,
-                        level: this.indentation,
-                    });
+            // Perform a regex match search for alphanumeric characters at the start of the
+            // current string. If we found something, we save the value as either a literal
+            // or a number, depending on an 'isNaN' check.
+            const charMatch = currChunk.match(CHARACTER_REGEX);
+            if (charMatch) {
+                const value = charMatch[0];
+                const type = isNaN(value) ? 'Literal' : 'Numeric';
+                tokens.push({
+                    type,
+                    value,
+                    row: this.row,
+                    column,
+                    level: this.indentation,
+                });
 
-                    currChunk = currChunk.slice(value.length);
-                    column += value.length;
-                }
+                currChunk = currChunk.slice(value.length);
+                column += value.length;
+            }
 
-                // Perform a regex match search for MUMPS string literals. The rub here is that
-                // literal quotation marks can be represented in a MUMPS string literal by 2
-                // consecutive double-quote marks. To mitigate this feature, once we find a group
-                // of double-quotes, we capture until we find a matching set (that is, the quote
-                // count become even). Then whatever we've captured until then is a string.
-                const stringMatch = currChunk.match(STRING_REGEX);
-                if (stringMatch) {
-                    let value = stringMatch[0];
-                    let quoteCount = value.length;
-                    currChunk = currChunk.slice(value.length);
+            // Perform a regex match search for MUMPS string literals. The rub here is that
+            // literal quotation marks can be represented in a MUMPS string literal by 2
+            // consecutive double-quote marks. To mitigate this feature, once we find a group
+            // of double-quotes, we capture until we find a matching set (that is, the quote
+            // count become even). Then whatever we've captured until then is a string.
+            const stringMatch = currChunk.match(STRING_REGEX);
+            if (stringMatch) {
+                let value = stringMatch[0];
+                let quoteCount = value.length;
+                currChunk = currChunk.slice(value.length);
 
-                    while (quoteCount % 2 !== 0) {
-                        if (currChunk.length === 0) {
-                            throw new Error(`Mismatched string error, line ${this.row}: ${chunk}`);
-                        }
-
-                        const endStringMatch = currChunk.match(STRING_REGEX);
-                        if (endStringMatch) {
-                            const endValue = endStringMatch[0];
-                            quoteCount += endValue.length;
-                            value += endValue;
-                            currChunk = currChunk.slice(endValue.length);
-                        }
-                        else {
-                            value += currChunk[0];
-                            currChunk = currChunk.slice(1);
-                        }
+                while (quoteCount % 2 !== 0) {
+                    if (currChunk.length === 0) {
+                        throw new Error(`Mismatched string error, line ${this.row}: ${chunk}`);
                     }
 
-                    tokens.push({
-                        type: 'String',
-                        value: value.slice(1, -1),
-                        row: this.row,
-                        column,
-                        level: this.indentation,
-                    })
-                    column += value.length;
+                    const endStringMatch = currChunk.match(STRING_REGEX);
+                    if (endStringMatch) {
+                        const endValue = endStringMatch[0];
+                        quoteCount += endValue.length;
+                        value += endValue;
+                        currChunk = currChunk.slice(endValue.length);
+                    }
+                    else {
+                        value += currChunk[0];
+                        currChunk = currChunk.slice(1);
+                    }
                 }
 
-                const spaceMatch = (currChunk[0] === ' ');
-                if (spaceMatch) {
-                    tokens.push({
-                        type: 'Separator',
-                        value: '',
-                        row: this.row,
-                        column,
-                        level: this.indentation,
-                    });
-                    currChunk = '';
-                    column++;
-                }
-
-                if (!operatorMatch && !stringMatch && !charMatch && !spaceMatch) {
-                    throw new Error(`Invalid syntax error, line ${this.row}: ${this.text} ${JSON.stringify(tokens)}`);
-                }
+                tokens.push({
+                    type: 'String',
+                    value: value.slice(1, -1),
+                    row: this.row,
+                    column,
+                    level: this.indentation,
+                })
+                column += value.length;
             }
-            return tokens;
-        });
 
-        this.tokens = _.flatten(this.tokens);
+            const spaceMatch = (currChunk[0] === ' ');
+            if (spaceMatch) {
+                tokens.push({
+                    type: 'Separator',
+                    value: '',
+                    row: this.row,
+                    column,
+                    level: this.indentation,
+                });
+                currChunk = '';
+                column++;
+            }
+
+            if (!operatorMatch && !stringMatch && !charMatch && !spaceMatch) {
+                throw new Error(`Invalid syntax error, line ${this.row}: ${this.text} ${JSON.stringify(tokens)}`);
+            }
+        }
+        return tokens;
     }
 }
 
